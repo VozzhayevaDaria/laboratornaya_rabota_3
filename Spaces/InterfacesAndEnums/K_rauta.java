eeeecdad77bf7bcf601a40cb484a675c2bda5fbd
VozzhayevaDaria <113607315+VozzhayevaDaria@users.noreply.github.com>
2023-06-09 19:56:39 +0300

    Add files via upload

---
+++ Spaces/InterfacesAndEnums/K_rauta.java
@@ -0,0 +1,31 @@
+package Spaces.InterfacesAndEnums;
+
+public class K_rauta{
+
+    public enum WallsMaterial {
+        WALLPAPERS,
+        PAINT,
+        PLASTER,
+        LINING,
+        CONCRETE
+    }
+    public enum FloorMaterial{
+        PARQUET,
+        LAMINATE,
+        LINOLEUM,
+        CORK,
+        TILE,
+        CONCRETE
+    }
+    public enum Color {
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        LIGTH_BLUE,
+        BLUE,
+        PURPLE,
+        GREY
+    }
+
+}
---
+++ Spaces/InterfacesAndEnums/LivingSpace.java
@@ -0,0 +1,8 @@
+package Spaces.InterfacesAndEnums;
+
+import Caracters.Human;
+
+public interface LivingSpace {
+    public void fallAsleep(Human person);
+    public void celebrateNY(Human cooker, Human... persons);
+}
